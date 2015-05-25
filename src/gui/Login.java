d032374bd6c7baf5e8ed47c6139c2b7b1700d562
Victor <Victor@Victor-PC>
2015-05-25 10:47:30 -0300

    Tentativas de Login

--- src/gui/Login.java
+++ src/gui/Login.java
@@ -83,8 +83,12 @@ public class Login extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        String usuario, senha;
-        senha = null;
+        String usuario;
+        String senha;
+
+        String senhadigitada;
+        senhadigitada = String.valueOf(jTextFieldSenha);
+
         try {
 
             Class.forName("com.mysql.jdbc.Driver");
@@ -93,34 +97,33 @@ public class Login extends javax.swing.JFrame {
             java.sql.Statement st = conn.createStatement();
             st.executeQuery("Select * FROM Logins WHERE usuario='" + this.jTextFieldUsuario.getText() + "'");
             ResultSet rs = st.getResultSet();
-            senha.equals(jTextFieldSenha);
-            
-            if (rs.equals(senha)){
-            JOptionPane.showMessageDialog(rootPane, "TITICA");
-            
+
+            senha = null;
+            while (rs.next()) {
+                usuario = rs.getString("usuario");
+                senha = rs.getString("senha");
             }
-            
-            
-//            while (rs.next()){
 
-//                jTextFieldSenha.setText(rs.getString("senha"));
-        
+            if (senha == senhadigitada) {
 
-    }
-    catch (SQLException | ClassNotFoundException e
+                JOptionPane.showMessageDialog(rootPane, "Logado!");
 
-    
-        ) {
+            } else {
+                JOptionPane.showMessageDialog(rootPane, senha);
+
+            }
+
+        } catch (SQLException | ClassNotFoundException e) {
             JOptionPane.showMessageDialog(rootPane, e);
-    }
+        }
 
 
     }//GEN-LAST:event_jButton1ActionPerformed
 
-/**
- * @param args the command line arguments
- */
-public static void main(String args[]) {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -131,32 +134,21 @@ public static void main(String args[]) {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
-                
 
-}
+                }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Login.class  
-
-.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } 
-
-catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Login.class  
-
-.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } 
-
-catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Login.class  
-
-.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } 
-
-catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Login.class  
-
-.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Login.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Login.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Login.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Login.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
