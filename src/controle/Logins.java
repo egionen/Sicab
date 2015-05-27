ad0b3a92ba53f5381f61a69f789f7339d47388f3
Victor <Victor@Victor-PC>
2015-05-27 09:13:05 -0300

    Tentativas de Erro

---
+++ src/controle/Logins.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controle;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author 14214290002
+ */
+@Entity
+@Table(name = "Logins")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Logins.findAll", query = "SELECT l FROM Logins l"),
+    @NamedQuery(name = "Logins.findByUsuario", query = "SELECT l FROM Logins l WHERE l.usuario = :usuario"),
+    @NamedQuery(name = "Logins.findBySenha", query = "SELECT l FROM Logins l WHERE l.senha = :senha")})
+public class Logins implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "usuario")
+    private String usuario;
+    @Basic(optional = false)
+    @Column(name = "senha")
+    private String senha;
+
+    public Logins() {
+    }
+
+    public Logins(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public Logins(String usuario, String senha) {
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (usuario != null ? usuario.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Logins)) {
+            return false;
+        }
+        Logins other = (Logins) object;
+        if ((this.usuario == null && other.usuario != null) || (this.usuario != null && !this.usuario.equals(other.usuario))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "controle.Logins[ usuario=" + usuario + " ]";
+    }
+    
+}
